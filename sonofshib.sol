4eb6b1da067363700cc28c955efc1cf64bab191c
sonofshib <92558784+sonofshib@users.noreply.github.com>
2021-10-14 22:06:49 -0400

    removed some debug comments
    removed some comments

--- sonofshib.sol
+++ sonofshib.sol
@@ -465,10 +465,8 @@ contract MiniShibaToken is Context, IERC20, Ownable {
         _rOwned[_msgSender()] = _rTotal;
         
         IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
-        //IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0xD99D1c33F9fC3444f8101754aBC46c52416550D1);
         
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory()).createPair(address(this), _uniswapV2Router.WETH());
-        //uniswapV2Pair = IUniswapV2Factory(0x6725F303b657a9451d8BA641348b6761A6CC7a17).createPair(address(this), _uniswapV2Router.WETH());
 
         uniswapV2Router = _uniswapV2Router;
 
